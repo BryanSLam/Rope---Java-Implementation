5993ace5c08e9ea455df3a5afdd9c82fc070ffb7
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-08-02 14:34:19 -0700

    Consolidated classes into one class

--- Rope/src/Rope.java
+++ Rope/src/Rope.java
@@ -50,7 +50,10 @@ public class Rope
 	}
 	
 	//Split: (i,s) splits s into two new strings s1 and s2, s1 = c1...ci, s2=ci+1...clast
-	
+	public Tuple<String, String> split(int i, String s)
+	{
+		return null;
+	}
 	
 	//Call the recursive method starting at the root with i
 	
@@ -60,4 +63,88 @@ public class Rope
 	{
 		return index(index, root);
 	}
+	
+//------------------------------------------------------------------------------------------------
+	
+	//I've been doing a lot of python and tuples are fantastic
+	//Took this from a stack overflow question for quick use
+
+	public class Tuple<X, Y> 
+	{ 
+	  public final X x; 
+	  public final Y y; 
+	  public Tuple(X x, Y y)
+	  { 
+	    this.x = x; 
+	    this.y = y; 
+	  } 
+	} 
+	
+//----------------------------------------------------------------------------------------------	
+	
+	//Holds methods specific to when you have access to a certain node and not searching through
+	//the rope for a specific node
+
+	//Confused deciding whether or not getWeight should stay in Node or in Rope like how I put
+	//the non recursive method of index in rope.
+
+	public class Node 
+	{
+		private String string;
+		private Node leftNode;
+		private Node rightNode;
+		
+		public Node(String s)
+		{
+			string = s;
+		}
+		
+		
+		public void addChild(Node n)
+		{
+			
+		}
+		public String getString()
+		{
+			return string;
+		}
+		
+		//To calculate the weight, get the weight of the node's left child and the weight of all
+		//the nodes in it's right subtree
+		
+		//Recursive method
+		private int weight(Node n)
+		{
+			if(n.getRight() == null)
+			{
+				return n.getString().length();
+			}
+			return n.getString().length() + weight(n.getRight());
+		}
+		
+		//To get the weight you have to call the getWeight starting with the first node on the left subtree
+		public int getWeight()
+		{
+			//If there's no left subtree, return the length of the string
+			if(this.leftNode == null)
+			{
+				return string.length();
+			}
+			return weight(this.leftNode);
+		}
+		
+
+		
+		
+		
+		public Node getLeft()
+		{
+			return leftNode;
+		}
+		public Node getRight()
+		{
+			return rightNode;
+		}
+	}
+
 }
